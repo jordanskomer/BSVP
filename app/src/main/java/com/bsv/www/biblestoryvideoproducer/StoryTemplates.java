d2bc5b5d39f1b98c2f59ae838c84c344b664b407
jordanskomer <jordan.skomer@gmail.com>
2015-10-11 10:15:15 -0500

    Initial Commit of ComCheckFrag, ConCheckFrag, TransFrag and their layouts

--- app/src/main/java/com/bsv/www/biblestoryvideoproducer/StoryTemplates.java
+++ app/src/main/java/com/bsv/www/biblestoryvideoproducer/StoryTemplates.java
@@ -3,66 +3,68 @@ package com.bsv.www.biblestoryvideoproducer;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.content.res.TypedArray;
-import android.os.PersistableBundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 
 public class StoryTemplates extends AppCompatActivity {
-    ListView mDrawerList;
+    private ListView mDrawerList;
     private ActionBarDrawerToggle mDrawerToggle;
     private DrawerLayout mDrawerLayout;
-    ArrayList<NavItem> mNavItems = new ArrayList<>();
+    private ArrayList<NavItem> mNavItems = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story_templates);
-
-        String[] aNavTitles  = getResources().getStringArray(R.array.nav_labels);
+        String[] aNavTitles = getResources().getStringArray(R.array.nav_labels);
         TypedArray aNavIcons = getResources().obtainTypedArray(R.array.nav_icons);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        for(int i = 0; i < aNavTitles.length; i++){
+        final String mActivityTitle = getTitle().toString();
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.nav_layout);
+        mDrawerList = (ListView) findViewById(R.id.nav_list);
+
+        for (int i = 0; i < aNavTitles.length; i++) {
             mNavItems.add(new NavItem(aNavTitles[i], aNavIcons.getResourceId(i, -1)));
         }
         aNavIcons.recycle();
 
-        mDrawerLayout = (DrawerLayout)findViewById(R.id.nav_layout);
-        mDrawerList = (ListView) findViewById(R.id.nav_list);
-
-        NavItemAdapter adapter = new NavItemAdapter(this, mNavItems);
+        NavItemAdapter adapter = new NavItemAdapter(getApplicationContext(), mNavItems);
         mDrawerList.setAdapter(adapter);
-        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.nav_open, R.string.title_activity_story_templates) {
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
             @Override
             public void onDrawerClosed(View drawerView) {
                 super.onDrawerClosed(drawerView);
+                getSupportActionBar().setTitle(mActivityTitle);
                 invalidateOptionsMenu();
             }
 
             @Override
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Pages");
                 invalidateOptionsMenu();
             }
         };
         mDrawerLayout.setDrawerListener(mDrawerToggle);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeButtonEnabled(true);
     }
 
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
         boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
         menu.findItem(R.id.menu_search).setVisible(!drawerOpen);
-        menu.findItem(R.id.menu_info).setVisible(!drawerOpen);
         return super.onPrepareOptionsMenu(menu);
     }
 
@@ -80,6 +82,12 @@ public class StoryTemplates extends AppCompatActivity {
     }
 
     @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
@@ -88,20 +96,59 @@ public class StoryTemplates extends AppCompatActivity {
         if(mDrawerToggle.onOptionsItemSelected(item)){
             return true;
         }
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.menu_info) {
-            Intent intent;
-            intent = new Intent(getApplicationContext(), InfoPage.class);
-            startActivity(intent);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+            return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public void onPostCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onPostCreate(savedInstanceState, persistentState);
-        mDrawerToggle.syncState();
+    //CREATED CLASSES
+    private void setupNavDrawer(){
+        String[] aNavTitles = getResources().getStringArray(R.array.nav_labels);
+        TypedArray aNavIcons = getResources().obtainTypedArray(R.array.nav_icons);
+        final String mActivityTitle = getTitle().toString();
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.nav_layout);
+        mDrawerList = (ListView) findViewById(R.id.nav_list);
+
+        for (int i = 0; i < aNavTitles.length; i++) {
+            mNavItems.add(new NavItem(aNavTitles[i], aNavIcons.getResourceId(i, -1)));
+        }
+        aNavIcons.recycle();
+
+        NavItemAdapter adapter = new NavItemAdapter(this, mNavItems);
+        mDrawerList.setAdapter(adapter);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close) {
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Pages");
+                invalidateOptionsMenu();
+            }
+        };
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
     }
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            Toast.makeText(StoryTemplates.this, Integer.toString(position), Toast.LENGTH_SHORT).show();
+//            displayView(position);
+            mDrawerLayout.closeDrawer(mDrawerList);
+        }
+    }
+    protected void displayView(int pos){
+        switch (pos){
+            case 1:
+                Intent intent = new Intent(StoryTemplates.this, InfoPage.class);
+                startActivity(intent);
+        }
+    }
+
 }
 
